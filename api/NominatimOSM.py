857c136663d646ebd20b26bf1c14b1475ac3c8f8
leonchief <doomaadon@gmail.com>
2026-02-03 11:27:09 +0300

    final 0.1

--- api/NominatimOSM.py
+++ api/NominatimOSM.py
@@ -5,9 +5,11 @@ import pytest
 
 class NominatimOSM:
 
+    @allure.step("URL: {base_url}")
     def __init__(self, base_url: str) -> None:
         self.base_url = base_url
 
+    @allure.step("Запрос статуса сервера в формате json")
     def get_status_Nominatim_json(self, headers: dict = None) -> dict:
 
         path = "{nominatim}status?format=json".format(nominatim=self.base_url)
@@ -16,6 +18,7 @@ class NominatimOSM:
 
         return resp.json()
 
+    @allure.step("Запрос статуса сервера в формате text")
     def get_status_Nominatim_text(self, headers: dict = None) -> str:
 
         path = "{nominatim}status?format=text".format(nominatim=self.base_url)
@@ -24,8 +27,8 @@ class NominatimOSM:
 
         return resp.text
 
-
-    def get_search_parametrize(self, q, format, polygon, addressdetails, limit, headers: dict = None) -> dict:
+    @allure.step("Поиск в параметризации по параметрам: {q}, {format}, {polygon}, {addressdetails}, {limit}")
+    def get_search_parametrize(self, q: str, format: str, polygon: int, addressdetails: int, limit: int, headers: dict = None) -> dict:
 
         params = {
             'q': q,
@@ -44,8 +47,8 @@ class NominatimOSM:
         else:
             return resp.json()
 
-
-    def get_reverse_parametrize(self, format, lat, lon, zoom, addressdetails, headers: dict = None) -> dict:
+    @allure.step("Реверс с параметризациией по парамтерам: {format}, {lat}, {lon}, {zoom}. {addressdetails}")
+    def get_reverse_parametrize(self, format: str, lat: int, lon: int, zoom: int, addressdetails: int, headers: dict = None) -> dict:
 
         params = {
             'format': format,
--- test/api/api_nominatim_test.py
+++ test/api/api_nominatim_test.py
@@ -3,51 +3,72 @@ import pytest
 
 from api.NominatimOSM import NominatimOSM
 
-@pytest.mark.skip
-def test_status_nominatim_json(api_client: NominatimOSM, test_data):
-    headers = {
-        "User-Agent": test_data.get_user_agent(),
-        "Accept": test_data.get_accept()
-    }
 
-    test = api_client.get_status_Nominatim_json(headers)
-    print(test)
+@allure.epic("openstreetmap")
+@allure.severity("blocker")
+class Test_Api_Nominatim_test_status:
 
-@pytest.mark.skip
-def test_status_nominatim_text(api_client: NominatimOSM, test_data):
-    headers = {
-        "User-Agent": test_data.get_user_agent(),
-        "Accept": test_data.get_accept()
-    }
+    @allure.feature("nominatim")
+    @allure.story("статус сервера")
+    @allure.title("статус по json")
+    def test_status_nominatim_json(self, api_client: NominatimOSM, test_data):
+        headers = {
+            "User-Agent": test_data.get_user_agent(),
+            "Accept": test_data.get_accept()
+        }
 
-    test = api_client.get_status_Nominatim_text(headers)
-    print(test)
+        test = api_client.get_status_Nominatim_json(headers)
+        allure.attach(f"Ответ {test}", name="Состояние сервера в json формате", attachment_type=allure.attachment_type.JSON)
 
-@pytest.mark.parametrize('q, formant, polygon, addressdetails, limit', [
-        ('Barbados', 'xml', '1', '1', '1'),
-        ('Tuvalu', 'json', '0', '1', '1'),
-        ('Chile', 'jsonv2', '0', '1', '1'),
-        ('Afghanistan', 'geojson', '1', '1', '1'),
-        ('Honduras', 'geocodejson', '1', '1', '1'),
-    ])
-def test_search_ddt(api_client: NominatimOSM, test_data, q, formant, polygon, addressdetails, limit):
-    headers = {
-        "User-Agent": test_data.get_user_agent(),
-        "Accept": test_data.get_accept()
-    }
-    api_client.get_search_parametrize(q, formant, polygon, addressdetails, limit, headers)
+    @allure.feature("nominatim")
+    @allure.story("статус сервера")
+    @allure.title("статус по text")
+    def test_status_nominatim_text(self, api_client: NominatimOSM, test_data):
+        headers = {
+            "User-Agent": test_data.get_user_agent(),
+            "Accept": test_data.get_accept()
+        }
+
+        test = api_client.get_status_Nominatim_text(headers)
+        allure.attach(f"Ответ {test}", name="Состояние сервера в текстовом формате", attachment_type=allure.attachment_type.TEXT)
+
+@allure.epic("openstreetmap")
+@allure.severity("blocker")
+class Test_Api_Nominatim_test_parametrize:
+
+    @pytest.mark.parametrize('q, format, polygon, addressdetails, limit', [
+            ('Barbados', 'xml', '1', '1', '1'),
+            ('Tuvalu', 'json', '0', '1', '1'),
+            ('Chile', 'jsonv2', '0', '1', '1'),
+            ('Afghanistan', 'geojson', '1', '1', '1'),
+            ('Honduras', 'geocodejson', '1', '1', '1'),
+        ])
+    @allure.feature("nominatim")
+    @allure.story("Прямое геокодирование по параметризации (ddt)")
+    @allure.title("Получения полного списка стран по параматризации search")
+    def test_search_ddt(self, api_client: NominatimOSM, test_data, q, format, polygon, addressdetails, limit):
+        headers = {
+            "User-Agent": test_data.get_user_agent(),
+            "Accept": test_data.get_accept()
+        }
+        ddt = api_client.get_search_parametrize(q, format, polygon, addressdetails, limit, headers)
+        allure.attach(f"Ответ {ddt}", name="Поиск параматризации", attachment_type=allure.attachment_type.JSON)
 
 
-@pytest.mark.parametrize('format, lat, lon, zoom, addressdetails', [
-        ('xml', '-9.911374', '-53.580959', '18', '1'),
-        ('json', '82.689336', '-31.809395', '50', '1'),
-        ('jsonv2', '-24.743005', '-32.115876', '30', '1'),
-        ('geojson', '10.239005', '35.390495', '22', '1'),
-        ('geocodejson', '-37.164761', '45.394554', '1', '1'),
-    ])
-def test_reverse_ddt(api_client: NominatimOSM, test_data, format, lat, lon, zoom, addressdetails):
-    headers = {
-        "User-Agent": test_data.get_user_agent(),
-        "Accept": test_data.get_accept()
-    }
-    api_client.get_reverse_parametrize(format, lat, lon, zoom, addressdetails, headers)
+    @pytest.mark.parametrize('format, lat, lon, zoom, addressdetails', [
+            ('xml', '-9.911374', '-53.580959', '18', '1'),
+            ('json', '82.689336', '-31.809395', '50', '1'),
+            ('jsonv2', '-24.743005', '-32.115876', '30', '1'),
+            ('geojson', '10.239005', '35.390495', '22', '1'),
+            ('geocodejson', '-37.164761', '45.394554', '1', '1'),
+        ])
+    @allure.feature("nominatim")
+    @allure.story("Обратные геокодирование по параметризации (ddt)")
+    @allure.title("Получения полного списка стран по параматризации reverse")
+    def test_reverse_ddt(self, api_client: NominatimOSM, test_data, format, lat, lon, zoom, addressdetails):
+        headers = {
+            "User-Agent": test_data.get_user_agent(),
+            "Accept": test_data.get_accept()
+        }
+        ddt = api_client.get_reverse_parametrize(format, lat, lon, zoom, addressdetails, headers)
+        allure.attach(f"Ответ {ddt}", name="Поиск параматризации", attachment_type=allure.attachment_type.JSON)
